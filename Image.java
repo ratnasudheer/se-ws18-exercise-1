f1fa5fe18998b4ed5585d22b5f35fb5b3c6559d7
ratnasudheer <38561355+ratnasudheer@users.noreply.github.com>
2018-11-01 03:41:30 +0530

    created and completed the project
    Ratna Sudheer Gondela- 119498
    Mohammed Shareef -119492

---
+++ Image.java
@@ -0,0 +1,64 @@
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+//Created Image class
+public class Image {
+	
+	 int width ;
+	 int height;
+	public byte[]data;
+	
+// creating a public byte array to store 24-bit RGB(3 byte per pixel	
+public Image() {
+	int height;
+	int width;
+		this.width = width;
+		this.height =height;
+		// Multiplying the  RGB with 3
+		data = new byte[width*height*3];
+		int pixelvalue = width*height;
+		int bytesvalue = pixelvalue*3;
+		this.data = new byte[bytesvalue];
+		
+	}
+
+//Created a method named as set which sets a single pixel at
+//given position represented by value
+public void set() {
+	int x; int y; int value;
+	
+	// split the 4 byte integer into its RGB
+	
+	int red = (value & 0xFF0000)>>4*4;
+	int green = (value & 0x00FF00)>>2*4;
+        int blue = (value & 0x0000FF);
+       
+        System.out.println("r: " + red + ", g: " + green + ", b: " + blue);
+			int position = (x+ y*width)*3;
+			data[position] = (byte) red;
+			data[position+1] = (byte)green;
+			data[position+2] = (byte)blue;
+			
+			
+		}
+// Created a method name as write that writes the image data into file
+public void write(String filename) throws Exception {
+	 String header;
+	FileOutputStream out = new FileOutputStream(filename);
+	 try {
+	
+	 out.write(new String(header).getBytes());
+	 out.close();
+		
+} catch (IOException e) {
+	
+	e.printStackTrace();
+	}
+}
+}
+
+
+
+ 
+
